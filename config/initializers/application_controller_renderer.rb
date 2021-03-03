23dbaed414d212d44556e68b83d8696b2dd109ae
ymasaki-dot <y.masaki0526@gmail.com>
2021-03-03 09:29:37 +0000

    [update] 3章終わり

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c89baf0de40594dc5d3116a9daa27f4149fd87fead15ee073255c882b1de361d2a23e33feb59663f7ab966c0366a91b7aaf357b6632dbb8ad925ba6bb359144c'
